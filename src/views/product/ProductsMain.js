9267f03393981e66b49b337767ed575984af2982
priyansh <shahpriyansh902705@gmail.com>
2023-12-08 19:18:37 +0530

    Update  dollar sign
    Update  dollar sign

--- src/views/product/ProductsMain.js
+++ src/views/product/ProductsMain.js
@@ -75,7 +75,7 @@ const CandidateRows = ({ product, i, navigate, deleteProductById, updateProduct 
                 {product.color}
             </TableCell>
             <TableCell align="center" style={{ paddingLeft: 16 }}>
-                ₹{product.price}
+                ${product.price}
             </TableCell>
             <TableCell align="center" style={{ paddingLeft: 16 }}>
                 <Switch color="secondary" checked={checked} onChange={handleChange} inputProps={{ 'aria-label': 'controlled' }} />
