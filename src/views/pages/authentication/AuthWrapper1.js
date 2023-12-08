ebdf7ad45d50b9d2ae087510eef9d2a9030596b2
pawarshivamd <shivam.p@konzeptsolutions.com>
2023-12-08 12:45:50 +0530

    --changes-index

--- src/views/pages/authentication/AuthWrapper1.js
+++ src/views/pages/authentication/AuthWrapper1.js
@@ -1,10 +1,14 @@
 // material-ui
 import { styled } from '@mui/material/styles';
+// import bg from "../../../assets/images/auth/bgl.png";
 
 // ==============================|| AUTHENTICATION 1 WRAPPER ||============================== //
 
 const AuthWrapper1 = styled('div')(({ theme }) => ({
     backgroundColor: theme.palette.primary.light,
+    // backgroundImage: bg,
+    // backgroundPosition: 'center',
+    // backgroundRepeat: 'no-repeat',
     minHeight: '100vh'
 }));
 
